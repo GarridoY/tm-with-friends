72c16f03246b3ceefe23082ceb2dbc999091cb3e
GarridoY <33898925+GarridoY@users.noreply.github.com>
2025-01-15 23:56:05 +0100

    Removed unused page for player ID lookup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,11 +37,6 @@ export default function RootLayout({
                 </Link>
               </li>
               <li className="text-center min-w-full lg:min-w-fit lg:border-none border-b border-b-slate-400">
-                <Link href="/player" className={`lg:min-w-fit min-w-full ${navigationMenuTriggerStyle()}`}>
-                    Player lookup
-                </Link>
-              </li>
-              <li className="text-center min-w-full lg:min-w-fit lg:border-none border-b border-b-slate-400">
                 <Link href="/map" className={`lg:min-w-fit min-w-full ${navigationMenuTriggerStyle()}`}>
                     Map lookup
                 </Link>
